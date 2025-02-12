bfd6d8ee15a09d52898c0a39952721da4c05199f
Yogalakshmi <yogarajakanibe@gmail.com>
2025-02-12 15:15:55 +0530

    express server setup done

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+// to install express 
+const express = require('express');
+// to create app in express
+const app=express();
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{
   console.log("Connnect to MongoDB");
+  app.listen(3001,()=>{
+    console.log("Server started on http://localhost:3001");
+  });
 })
 .catch(err=>{
   console.log("error in connection in mongodb",err);
