1bd8238f4212efa06e7fc5bb2b65087a4a061f92
Yogalakshmi <yogarajakanibe@gmail.com>
2025-02-14 14:54:11 +0530

    .env changes updated

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 // Only  for db connectivity and server issues
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 // to import application code here
-const app= require('./app');
+const app = require("./app");
 
 // for database connection
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
-  console.log("Connnect to MongoDB");
-  app.listen(3001,()=>{
-    console.log("Server started on http://localhost:3001");
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connnect to MongoDB");
+
+    // Start the server
+    app.listen(3001, () => {
+      console.log("Server started on http://localhost:3001");
+    });
+  })
+  .catch((err) => {
+    console.log("error in connection in mongodb", err);
   });
-})
-.catch(err=>{
-  console.log("error in connection in mongodb",err);
-})
