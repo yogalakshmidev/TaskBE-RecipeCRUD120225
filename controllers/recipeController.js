c01156d955743044d48520b08f6b035f30a2a561
Yogalakshmi <yogarajakanibe@gmail.com>
2025-02-14 14:13:50 +0530

    CRUD completed for RecipeApp

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,60 +1,83 @@
 // import model here
-const Recipe = require('../models/recipe');
+const Recipe = require("../models/recipe");
 
+const recipeController = {
+  createRecipe: async (req, res) => {
+       try {
+        // get the data from the request body
+      const { Title, Ingredients, Content } = req.body;
 
-const recipeController={
-createRecipe:async (req, res) => {
-  // console.log(req.body);
-  // req.json({ message: "Tasty Recipe World" });
-  try {
-    const { Title , Ingredients , Content } = req.body;
-    // Create a new Recipe
-    const newRecipe = new Recipe({
-      Title ,
-      Ingredients ,
-      Content 
-    });
+      // Create a new Recipe
+      const newRecipe = new Recipe({
+        Title,
+        Ingredients,
+        Content,
+      });
 
-    // Save the post in the database
-    await newRecipe.save();
+      // Save the post in the database
+      await newRecipe.save();
 
-    // send the response to the frontend
-    res.status(201).json({ message : 'Recipe created successfully.'});
-  } catch (error) {
-    res.status(500).json({ message: error.message});
-  }
-},
-getRecipes: async(req,res) => {
-try{
-  // get all the recipes from the database
-  const recipes = await Recipe.find().select('-__v');
-
-  // send a response
-  res.status(200).json(recipes);
-}catch(error){
-  res.status(500).json({message: error.message});
-}
-},
-getRecipe: async(req,res) => {
-  try{
-  
-  }catch(error){
-    res.status(500).json({message: error.message});
-  }
-  },
-  updateRecipe: async(req,res) => {
-    try{
-    
-    }catch(error){
-      res.status(500).json({message: error.message});
+      // send the response to the frontend
+      res.status(201).json({ message: "Recipe created successfully." });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    },
-    deleteRecipe: async(req,res) => {
-      try{
-      
-      }catch(error){
-        res.status(500).json({message: error.message});
-      }
-      }
-}
+  },
+  getRecipes: async (req, res) => {
+    try {
+      // get all the recipes from the database
+      const recipes = await Recipe.find().select("-__v");
+
+      // send a response
+      res.status(200).json({recipes});
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+  getRecipe: async (req, res) => {
+    try {
+      // get the recipe id from the request parameter
+      const { id } = req.params;
+
+      // get the receipe from the database
+      const recipe = await Recipe.findById(id).select("-__v");
+
+      // send the response
+      res.status(200).json({ recipe });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+  updateRecipe: async (req, res) => {
+    try {
+      // get the recipe id from the request parameter
+      const { id } = req.params;
+
+      // get the data from the request body
+      const { Title, Ingredients, Content } = req.body;
+
+      // update the recipe
+      await Recipe.findByIdAndUpdate(id, { Title, Ingredients, Content });
+
+      // send the response
+      res.status(200).json({ message: 'Recipe Updated Successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+  deleteRecipe: async (req, res) => {
+    try {
+      // get the recipe id from the request parameter
+      const { id } = req.params;
+
+      // delete the recipe
+      await Recipe.findByIdAndDelete(id);
+
+      // send the response
+      res.status(200).json({ message: "Recipe deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+};
 module.exports = recipeController;
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,12 +1,11 @@
 // to create router use express
-const express = require("express");
-
+const express = require('express');
 
 //  import from Controller
 
 // const recipeController= require("../controllers/recipeController");
 
-const {createRecipe, getRecipes }=  require('../controllers/recipeController');
+const { createRecipe, getRecipes, getRecipe, updateRecipe, deleteRecipe }=  require('../controllers/recipeController');
 
 // create a new router
 const recipesRouter = express.Router();
@@ -14,4 +13,8 @@ const recipesRouter = express.Router();
 // recipesRouter.post('/',recipeController.createRecipe );
 recipesRouter.post('/',createRecipe);
 recipesRouter.get('/',getRecipes);
+recipesRouter.get('/:id', getRecipe);
+recipesRouter.put('/:id',updateRecipe);
+recipesRouter.delete('/:id',deleteRecipe);
+
 module.exports = recipesRouter;
